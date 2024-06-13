c503782654a68d8a780303919baa78593baaa7f4
Brayito <bryan.sepulveda@usach.cl>
2024-06-13 16:52:29 -0400

    Inicializacion

---
+++ ms-reports/ms-reports/src/test/java/autofix/ms_reports/MsReportsApplicationTests.java
@@ -0,0 +1,13 @@
+package autofix.ms_reports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsReportsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
